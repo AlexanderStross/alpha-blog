544f981654f23aa72a86504b6f66770275e50cbb
“abstross” <abstross@pm.me>
2021-07-01 17:43:27 -0500

    memoization and authenication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -14,13 +14,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
-      flash[:notice] = "Your account informatoin was successfully updated"
+      flash[:success] = "Your account informatoin was successfully updated"
       redirect_to articles_path
     else
       render 'edit'
@@ -30,7 +29,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfuly signed up"
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}, you have successfuly signed up"
+      session[:user_id] = @user.id
       redirect_to articles_path
     else
       render 'new'
@@ -41,5 +41,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
