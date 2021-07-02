77e4c78824698d7f0161a9476a00de3ef9f5dd7f
“abstross” <abstross@pm.me>
2021-07-02 13:32:59 -0500

    added restriction for logged in and not loggedin users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "logged in successfuly"
+      flash[:success] = "log in successful"
       redirect_to user
     else
       flash.now[:danger] = "There was something wrong with your login credentials"
